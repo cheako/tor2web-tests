0c1b6ad834713bbb04979fa8db6627d71fc695d2
Michael Mestnik <cheako+github_com@mikemestnik.net>
2017-10-12 15:59:12 -0500

    update depends

--- bin/.root-setup.sh
+++ bin/.root-setup.sh
@@ -4,5 +4,6 @@ apt-get -yq update &&
 	apt-get -yq --no-install-suggests --no-install-recommends \
 	--allow-downgrades --allow-remove-essential \
 	--allow-change-held-packages install \
-	libmojolicious-perl \
+	daemon socat libmojolicious-perl \
+	libclass-method-modifiers-perl
 
