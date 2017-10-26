f7f3fbf3a4513ccdd08d247449097f36f20bef4e
Michael Mestnik <cheako+github_com@mikemestnik.net>
2017-10-26 16:23:13 -0500

    Trace socat

--- bin/user-start.sh
+++ bin/user-start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 daemon --name=ttwhttpd --respawn --output=/tmp/ttwhttpd.log -- bin/ttwhttpd.pl daemon
-daemon --name=socatssl --respawn -- socat TCP-LISTEN:8081,fork OPENSSL:127.0.0.1:8444,cafile=etc/ssl/test-cert.pem,verify=0
+daemon --name=socatssl --respawn -E /dev/stderr -- strace -f -s 200 socat TCP-LISTEN:8081,fork OPENSSL:127.0.0.1:8444,cafile=etc/ssl/test-cert.pem,verify=0
