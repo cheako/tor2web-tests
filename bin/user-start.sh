35da238b95bae35a7312236cb6acd08a7e30664a
Michael Mestnik <cheako+github_com@mikemestnik.net>
2017-10-26 16:52:40 -0500

    try this

--- bin/user-start.sh
+++ bin/user-start.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 daemon --name=ttwhttpd --respawn --output=/tmp/ttwhttpd.log -- bin/ttwhttpd.pl daemon
-daemon --name=socatssl --respawn -E /dev/stderr -- strace -f -s 200 socat TCP-LISTEN:8081,fork OPENSSL:127.0.0.1:8444,cafile=etc/ssl/test-cert.pem,verify=0
+daemon --name=socatssl --respawn -f -- strace -f -s 200 socat TCP-LISTEN:8081,fork OPENSSL:127.0.0.1:8444,cafile=etc/ssl/test-cert.pem,verify=0&
+disown
