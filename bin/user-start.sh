aac395889e9991444eb1a399291116e88b7d1e05
Michael Mestnik <cheako+github_com@mikemestnik.net>
2017-11-02 16:28:30 -0500

    Send to syslog

--- bin/user-start.sh
+++ bin/user-start.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-daemon --name=ttwhttpd --respawn -- bin/ttwhttpd.pl daemon
-sleep 4
+daemon --name=ttwhttpd --respawn -l daemon.crit -b daemon.err -E daemon.crit -O daemon.err -- bin/ttwhttpd.pl daemon
